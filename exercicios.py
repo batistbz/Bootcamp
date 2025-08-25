0c13a84fb8fef44454b5c1b4f5c90fecb0f0bad7
Bea <biabatista1219@gmail.com>
2025-08-25 16:26:10 -0300

    alteracoes

--- exercicios.py
+++ exercicios.py
@@ -4,4 +4,4 @@ nome = "Beatriz"
 idade = 16
 comida = "Lasanha de frango com Molho branco"
 
-print("Meu nome é",nome, ",tenho",idade,"anos e minha comida favorita é",comida)
+print(f"Meu nome é {nome},tenho {idade} anos e minha comida favorita é {comida}")
