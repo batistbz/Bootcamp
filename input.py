4e06abaa6853a76a69ec364e614832ee44f89cc8
Bea <biabatista1219@gmail.com>
2025-08-25 16:53:44 -0300

    exerrrrr

--- input.py
+++ input.py
@@ -1,2 +1,6 @@
 nome = input("Qual seu nome? ") # primeiro é exibida certa mensagem, que espera uma resposta do usuário para continuar
 print(f"Boa tarde {nome}") # aqui a mensagem que aparece contém a resposta do usuário
+
+nominho = input("Qual seu nome? ")
+idade = input("Qual sua idade? ")
+print(f"O seu nome é {nominho} e você tem {idade} anos!")
