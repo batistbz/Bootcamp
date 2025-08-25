1c204f09f4c662c7157ad9b0807bba8a0005ea97
Bea <biabatista1219@gmail.com>
2025-08-25 16:16:44 -0300

    exercicio1

---
+++ exercicios.py
@@ -0,0 +1,7 @@
+# neste exercicio é necessario que escreva seu nome, idade e comida favorita
+
+nome = "Beatriz"
+idade = 16
+comida = "Lasanha de frango com Molho branco"
+
+print("Meu nome é", nome, ", tenho", idade, "anos e minha comida favorita é", comida)
