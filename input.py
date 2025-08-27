ad1aab69a7126d6d57062cae0ca55c63b8eae659
Bea <biabatista1219@gmail.com>
2025-08-27 16:14:24 -0300

    exclusaopy

--- input.py
+++
@@ -1,6 +0,0 @@
-nome = input("Qual seu nome? ") # primeiro é exibida certa mensagem, que espera uma resposta do usuário para continuar
-print(f"Boa tarde {nome}") # aqui a mensagem que aparece contém a resposta do usuário
-
-nominho = input("Qual seu nome? ")
-idade = input("Qual sua idade? ")
-print(f"O seu nome é {nominho} e você tem {idade} anos!")
