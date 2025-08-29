f63fd4423beee7b3279ed48a0c00a7fce719d1bf
Bea <biabatista1219@gmail.com>
2025-08-29 16:52:34 -0300

    tentando fazer

---
+++ ativ-react/src/components/Cards.jsx
@@ -0,0 +1,12 @@
+import twitter from '../assets/images/twitter.png'
+import instagram from '../assets/images/instagram.png'
+import youtube from '../assets/images/youtube.png'
+
+const cards = [
+    {
+        image:
+        altText:
+        title:
+        description:
+        link:
+    }
