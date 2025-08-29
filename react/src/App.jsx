4ada2f3dd6233a42b08490f9a38f30d27623c37e
Bea <biabatista1219@gmail.com>
2025-08-29 16:01:44 -0300

    finalizando

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import CourseSection from "./components/CoursesSection";
 import NewSection from "./components/NewSection"
 import ContactForm from "./components/ContactForm";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <CourseSection/>
     <NewSection/>
     <ContactForm/>
+    <Footer/>
     </main>
     </>
   );
