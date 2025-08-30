4866002a89226828a5286257e5173b374d8c6225
batistbz <biabatista1219@gmail.com>
2025-08-30 09:39:14 -0300

    tentando né

---
+++ ativ-react/src/components/CSection.jsx
@@ -0,0 +1,13 @@
+import twitter from '../assets/images/twitter.png'
+import instagram from '../assets/images/instagram.png'
+import youtube from '../assets/images/youtube.png'
+
+const cards = [
+    {
+        image: twitter,
+        altText: "Twitter",
+        title:"Twitter",
+        description: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+        link: "",
+    }
+];
