b220f09b295e8a4187e7f5ad8c203e66d0cbf5a9
batistbz <biabatista1219@gmail.com>
2025-08-30 10:45:10 -0300

    update

--- ativ-react/src/components/CSection.jsx
+++ ativ-react/src/components/CSection.jsx
@@ -8,6 +8,40 @@ const cards = [
         altText: "Twitter",
         title:"Twitter",
         description: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-        link: "",
+        link: "https://x.com/?lang=pt",
+    },
+    {
+        image: instagram,
+        altText: "Instagram",
+        title: "Instagram",
+        description: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+        link: "https://www.instagram.com/",
+    },
+    {
+        image: youtube,
+        altText: "Youtube",
+        title: "Youtube",
+        description: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+        link:"https://www.youtube.com/",
     }
 ];
+        function CSection (){
+            return (
+                <section id="cards" class="cards">
+            <div class="container">
+                <div class="cards">
+                    {course.map((cards) => (
+                        <CourseCard
+                             key={cards.title}
+                            image={cards.image}
+                            altText={cards.altText}
+                            title={cards.title}
+                            description={cards.description}
+                            link={cards.link}
+                            />
+                    ))}
+                </div>
+                </div>                
+                </section>
+            );
+        }
