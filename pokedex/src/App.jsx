491732682b67477ab9a00dac90366e8541d79d41
Bea <biabatista1219@gmail.com>
2025-09-01 15:56:29 -0300

    esqueci de dar commit

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -4,9 +4,16 @@ import './App.css'
 function App() {
   const [pokemons, setPokemons] = useState([])
   useEffect(() => {
-      const fetchPokemons = async;
+      const fetchPokemons = async () => {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=9");
+        const data = await response.json();
 
-
+        const detailedPokemon = await Promise.all(
+          data.results.map(async (pokemon) => {
+            const pokemonResponse = await fetch(pokemon.url);
+          })
+        );
+        };
       });
   return (
     <>
